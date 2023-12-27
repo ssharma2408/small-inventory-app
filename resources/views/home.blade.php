ff18defe43c997bd0bccaca8d00deef1b63cfe98
Tejas <tejas@mavenagency.co.za>
2023-12-27 15:50:59 +0530

    Dashboard Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,200 +1,229 @@
 @extends('layouts.admin')
 @section('content')
 <div class="content">
-    @if(\Auth::user()->roles()->first()->title != "Delivery Agent")
-		
-		@if(\Auth::user()->roles()->first()->title != "Sales Manager")
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="card">
-						<div class="card-header">
-							Admin Dashboard
-						</div>						
-						<div class="card-body">
-							<div class="row">
-								<div class="col-md-6">
-									<h4>Expenses</h4>
-									<ul>
-										<li><a href="{{ route('admin.reports.get_expense_report') }}?status=0">Open</a></li>
-										<li><a href="{{ route('admin.reports.get_expense_report') }}?status=1">Paid</a></li>
-										<li><a href="{{ route('admin.reports.get_expense_report') }}?status=2">Over Due</a></li>
-									</ul>
-								</div>
-								<div class="col-md-6">
-									<h4>Orders</h4>
-									<ul>
-										<li><a href="{{ route('admin.reports.get_order_report') }}?status=0">Open</a></li>
-										<li><a href="{{ route('admin.reports.get_order_report') }}?status=1">Paid</a></li>
-										<li><a href="{{ route('admin.reports.get_order_report') }}?status=2">Over Due</a></li>
-									</ul>
-								</div>
-							</div>
-							<hr/>
-							<div><h5>Total Open Orders: {{$admin['total_open_order']}}</h5></div>
-							@if(!empty($admin['accepted_order']))
-								<div class="mt-2 mb-2"><h5>Last 3 Accepted Order:</h5> 
-									<div class="row">
-										<div class="col-lg-6"><b>Order ID</b></div>
-										<div class="col-lg-6"><b>Order Amount</b></div>
-									</div>								
-									@foreach($admin['accepted_order'] as $ord)
-										<div class="row">
-											<div class="col-lg-6">{{$ord['id']}}</div>
-											<div class="col-lg-6">{{$ord['order_total']}}</div>
-										</div>
-									@endforeach
-								</div>
-							@endif
-							@if(!empty($admin['expenses']))
-								<div><h5>Last 3 Expense:</h5>
-									<div class="row">
-										<div class="col-lg-6"><b>Invoice Number</b></div>
-										<div class="col-lg-6"><b>Amount</b></div>
-									</div>
-									@foreach($admin['expenses'] as $exp)
-										<div class="row">
-											<div class="col-lg-6">{{$exp['invoice_number']}}</div>
-											<div class="col-lg-6">{{$exp['final_price']}}</div>
-										</div>
-									@endforeach									
-								</div>
-							@endif
-						</div>
-					</div>
+	@if(\Auth::user()->roles()->first()->title != "Delivery Agent")
+
+	@if(\Auth::user()->roles()->first()->title != "Sales Manager")
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="card">
+				<div class="card-header">
+					Admin Dashboard
 				</div>
-			</div>
-		@else
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="card">
-						<div class="card-header">
-							Sales Dashboard
-						</div>
-						<div class="card-body">
-							@if(!empty($sales['accepted_order']))
-								<div><h5>Last 3 Accepted Order:</h5> 
-									<div class="row">
-										<div class="col-lg-6"><b>Order ID</b></div>
-										<div class="col-lg-6"><b>Order Amount</b></div>
-									</div>								
-									@foreach($sales['accepted_order'] as $ord)
-										<div class="row">
-											<div class="col-lg-6">{{$ord['id']}}</div>
-											<div class="col-lg-6">{{$ord['order_total']}}</div>
-										</div>
-									@endforeach
-								</div>
-							@endif
-						</div>
+				<div class="card-body">
+					<div>
+						<h5>Total Open Orders: {{$admin['total_open_order']}}</h5>
 					</div>
-				</div>
-			</div>
-		@endif
-	
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="card">
-					<div class="card-header row">
-						<div class="col-lg-4">Module</div>
-						<div class="col-lg-4">Total</div>
-						<div class="col-lg-4">Link</div>
+					@if(!empty($admin['accepted_order']))
+					<div class="mt-2 mb-2">
+						<h5>Last 3 Accepted Order:</h5>
+						<div class="row">
+							<div class="col-lg-6"><b>Order ID</b></div>
+							<div class="col-lg-6"><b>Order Amount</b></div>
+						</div>
+						@foreach($admin['accepted_order'] as $ord)
+						<div class="row">
+							<div class="col-lg-6">{{$ord['id']}}</div>
+							<div class="col-lg-6">{{$ord['order_total']}}</div>
+						</div>
+						@endforeach
 					</div>
-					@if(\Auth::user()->roles()->first()->title != "Sales Manager")
-						<div class="card-body row">
-							<div class="col-lg-4">Category</div>
-							<div class="col-lg-4">{{$category['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.categories.index')}}">View All</a></div>
+					@endif
+					@if(!empty($admin['expenses']))
+					<div>
+						<h5>Last 3 Expense:</h5>
+						<div class="row">
+							<div class="col-lg-6"><b>Invoice Number</b></div>
+							<div class="col-lg-6"><b>Amount</b></div>
 						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Product</div>
-							<div class="col-lg-4">{{$product['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.products.index')}}">View All</a></div>
+						@foreach($admin['expenses'] as $exp)
+						<div class="row">
+							<div class="col-lg-6">{{$exp['invoice_number']}}</div>
+							<div class="col-lg-6">{{$exp['final_price']}}</div>
 						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Tax</div>
-							<div class="col-lg-4">{{$tax['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.taxes.index')}}">View All</a></div>
-						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Payment Method</div>
-							<div class="col-lg-4">{{$payment_method['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.payment-methods.index')}}">View All</a></div>
-						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Shrinkage</div>
-							<div class="col-lg-4">{{$shrinkage['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.shrinkages.index')}}">View All</a></div>
-						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Supplier</div>
-							<div class="col-lg-4">{{$supplier['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.suppliers.index')}}">View All</a></div>
-						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Expenses</div>
-							<div class="col-lg-4">{{$expense['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.inventories.index')}}">View All</a></div>
-						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Customers</div>
-							<div class="col-lg-4">{{$customer['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.customers.index')}}">View All</a></div>
-						</div>
-						<div class="card-body row">
-							<div class="col-lg-4">Orders</div>
-							<div class="col-lg-4">{{$order['total']}}</div>
-							<div class="col-lg-4"><a href="{{route('admin.orders.index')}}">View All</a></div>
-						</div>
-					@else
-					<div class="card-body row">
-						<div class="col-lg-4">Customers</div>
-						<div class="col-lg-4">{{$customer['total']}}</div>
-						<div class="col-lg-4"><a href="{{route('admin.customers.index')}}">View All</a></div>
-					</div>	
-					<div class="card-body row">
-						<div class="col-lg-4">Supplier</div>
-						<div class="col-lg-4">{{$supplier['total']}}</div>
-						<div class="col-lg-4"><a href="{{route('admin.suppliers.index')}}">View All</a></div>
-					</div>
-					<div class="card-body row">
-						<div class="col-lg-4">Product</div>
-						<div class="col-lg-4">{{$product['total']}}</div>
-						<div class="col-lg-4"><a href="{{route('admin.products.index')}}">View All</a></div>
+						@endforeach
 					</div>
 					@endif
 				</div>
 			</div>
 		</div>
-		@if(\Auth::user()->roles()->first()->title != "Sales Manager")
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="card">
-						<div class="card-header">
-							Reports
+	</div>
+	@else
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="card">
+				<div class="card-header">
+					Sales Dashboard
+				</div>
+				<div class="card-body">
+					@if(!empty($sales['accepted_order']))
+					<div>
+						<h5>Last 3 Accepted Order:</h5>
+						<div class="row">
+							<div class="col-lg-6"><b>Order ID</b></div>
+							<div class="col-lg-6"><b>Order Amount</b></div>
 						</div>
-						<div class="card-body">
-							<ul>						
-								<li><a href="{{ route('admin.reports.get_expense_report') }}">Purchase Report</a></li>
-								<li><a href="{{ route('admin.reports.get_order_report') }}">Order Report</a></li>
-							</ul>
+						@foreach($sales['accepted_order'] as $ord)
+						<div class="row">
+							<div class="col-lg-6">{{$ord['id']}}</div>
+							<div class="col-lg-6">{{$ord['order_total']}}</div>
 						</div>
+						@endforeach
 					</div>
+					@endif
 				</div>
 			</div>
+		</div>
+	</div>
+	@endif
+
+	@if(\Auth::user()->roles()->first()->title != "Sales Manager")
+	<h4 class="mb-4">Modules</h4>
+	<div class="row">
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-info">
+				<div class="inner">
+					<h3>16</h3>
+					<p>Category</p>
+				</div>
+				<a href="{{route('admin.categories.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-success">
+				<div class="inner">
+					<h3>{{$product['total']}}</h3>
+					<p>Product</p>
+				</div>
+				<a href="{{route('admin.products.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-warning">
+				<div class="inner">
+					<h3>{{$tax['total']}}</h3>
+					<p>Tax</p>
+				</div>
+				<a href="{{route('admin.taxes.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-danger">
+				<div class="inner">
+					<h3>{{$payment_method['total']}}</h3>
+					<p>Payment Method</p>
+				</div>
+				<a href="{{route('admin.payment-methods.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-danger">
+				<div class="inner">
+					<h3>{{$shrinkage['total']}}</h3>
+					<p>Shrinkage</p>
+				</div>
+				<a href="{{route('admin.shrinkages.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-gray">
+				<div class="inner">
+					<h3>{{$supplier['total']}}</h3>
+					<p>Supplier</p>
+				</div>
+				<a href="{{route('admin.suppliers.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-secondary">
+				<div class="inner">
+					<h3>{{$expense['total']}}</h3>
+					<p>Expenses</p>
+				</div>
+				<a href="{{route('admin.suppliers.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-info">
+				<div class="inner">
+					<h3>{{$customer['total']}}</h3>
+					<p>Customers</p>
+				</div>
+				<a href="{{route('admin.customers.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-primary">
+				<div class="inner">
+					<h3>{{$order['total']}}</h3>
+					<p>Orders</p>
+				</div>
+				<a href="{{route('admin.orders.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		@else
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-success">
+				<div class="inner">
+					<h3>{{$customer['total']}}</h3>
+					<p>Customers</p>
+				</div>
+				<a href="{{route('admin.customers.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-warning">
+				<div class="inner">
+					<h3>{{$supplier['total']}}</h3>
+					<p>Supplier</p>
+				</div>
+				<a href="{{route('admin.suppliers.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-6">
+			<div class="small-box bg-danger">
+				<div class="inner">
+					<h3>{{$product['total']}}</h3>
+					<p>Product</p>
+				</div>
+				<a href="{{route('admin.products.index')}}" class="small-box-footer">View All <i class="fas fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
 		@endif
+	</div>
+
+	@if(\Auth::user()->roles()->first()->title != "Sales Manager")
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="card">
+				<div class="card-header">
+					<h4 class="mb-0">Reports</h4>
+				</div>
+				<div class="card-footer p-0">
+					<ul class="nav flex-column">
+						<li class="nav-item"><a href="{{ route('admin.reports.get_expense_report') }}" class="nav-link">Purchase Report</a></li>
+						<li class="nav-item"><a href="{{ route('admin.reports.get_order_report') }}" class="nav-link">Order Report</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	@endif
 	@else
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="card">
-					<div class="card-header">
-						Dashboard
-					</div>
-					<div class="card-body">
-						<div><h5>Total Open Orders: {{$del_agent['total_assigned_orders']}}</h5></div>
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="card">
+				<div class="card-header">
+					Dashboard
+				</div>
+				<div class="card-body">
+					<div>
+						<h5>Total Open Orders: {{$del_agent['total_assigned_orders']}}</h5>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 	@endif
 </div>
 @endsection
