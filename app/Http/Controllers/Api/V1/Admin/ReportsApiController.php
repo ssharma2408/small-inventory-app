9d89d5e77f8c608315e0220648d9d96829938c0b
Sandeep Sharma <sandeep.sharma@maven.one>
2023-12-29 14:00:55 +0530

    Added credit note and reports api

---
+++ app/Http/Controllers/Api/V1/Admin/ReportsApiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Inventory;
+use App\Models\Order;
+use App\Models\Customer;
+use App\Models\Supplier;
+use App\Models\Product;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use DB;
+
+class ReportsApiController extends Controller
+{
+    public function get_expense_report(){
+		
+		$status = ['Due', 'Closed',  'Overdue'];
+
+		$inventories = Inventory::with(['supplier', 'media', 'payment'])->get();
+
+		$suppliers = Supplier::select('supplier_name', 'id')->get();
+
+		return response()->json([
+            'inventories' => $inventories,
+            'status'=> $status,
+            'suppliers'=> $suppliers			
+        ], 200);
+	}
+	
+	public function get_order_report(){
+		
+		$status = ['Due', 'Closed',  'Overdue'];
+		$orders = Order::with(['sales_manager', 'customer', 'payment'])->get();
+
+		$customers = Customer::select('name', 'id')->get();
+		
+		return response()->json([
+            'orders' => $orders,
+            'status'=> $status,
+            'customers'=> $customers,
+        ], 200);
+	}
+	
+	public function get_product_expiry_report(){
+		
+		$status = ['Due', 'Closed',  'Overdue'];
+		$expense_items = DB::table('expense_items')
+                ->join('products', 'expense_items.product_id', '=', 'products.id')
+                ->join('inventories', 'expense_items.expense_id', '=', 'inventories.id')
+                ->select('products.name', 'expense_items.product_id', 'expense_items.stock', 'expense_items.is_box', 'expense_items.purchase_price',  'products.box_size', 'expense_items.exp_date', 'inventories.invoice_number', 'inventories.id')
+                ->get();
+		
+		$products = Product::select('name', 'id')->get();
+
+		return response()->json([
+            'expense_items' => $expense_items,
+            'status'=> $status,
+            'products'=> $products
+        ], 200);
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,11 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', '
 	// Credit Note
     Route::apiResource('credit-notes', 'CreditNoteApiController');
 	
+	// Reports
+	Route::get('reports/get_expense_report', 'ReportsApiController@get_expense_report')->name('reports.get_expense_report');
+	Route::get('reports/get_order_report', 'ReportsApiController@get_order_report')->name('reports.get_order_report');
+	Route::get('reports/get_product_expiry_report', 'ReportsApiController@get_product_expiry_report')->name('reports.get_product_expiry_report');
+	
 	// Profile
 	Route::post('change_password','AuthController@change_password');
 });
