14a62ac773a52ff2fa6c16c0ba57467c3d4b21c5
Sandeep Sharma <sandeep.sharma@maven.one>
2023-12-29 14:47:52 +0530

    added User APi

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@ use App\Http\Controllers\Api\V1\Admin\AuthController;
 
 Route::post('/v1/login', [AuthController::class, 'login']);
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
-    // Supplier
+    // User
+    Route::apiResource('users', 'UserApiController');
+	
+	// Supplier
     Route::apiResource('suppliers', 'SupplierApiController');
 
     // Customers
