df35d1637047c0eeb615f3719d6652f8e904904a
Jagdish Parmar <jagdish@jagdishparmar.com>
2024-09-02 18:30:18 +0530

    invoiceupdate

--- resources/views/admin/orders/order_summary.blade.php
+++ resources/views/admin/orders/order_summary.blade.php
@@ -13,16 +13,19 @@
     }
 
     body {
+		
+		 font-family: 'Courier New', monospace;
 
         -webkit-font-smoothing: antialiased;
         -webkit-text-size-adjust: none;
-        width: 100% !important;
-        margin: 0 !important;
-        padding: 0 !important;
-        height: 100%;
+        width: 98% !important;
+        margin: 5px !important;
+        padding: 5px !important;
+        height: 98%;
         font-size: 20px !important;
+		
         color: #000000;
-        font-family: Arial, sans-serif;
+        //font-family: Arial, sans-serif;
     }
 
     table {
@@ -44,7 +47,14 @@
         margin-top: 0;
         margin-bottom: 0.5rem;
         font-size: inherit;
+		line-height: 1.5;
     }
+	 p.trueleaf {
+         
+		line-height: auto!important;
+    }
+	tr.item-row td { margin-top: 20px; border:1px solid #eee;
+	}
     </style>
 </head>
 
@@ -52,27 +62,25 @@
     <div class="invoice-box">
         <table style="width: 100%;" cellpadding="10">
             <tr>
-                <td style="text-align: left; width:230px;">
-                    <p><strong>True Leaf retail Inc.</strong></p>
-                    <p>U - 15, 6720 Davand Drive Mississauga ON L5T 2K7</p>
-                    <p>+1 4377555253</p>
-                    <p>
-                        <a href="mailto:trueleafretailinc@gmail.com">trueleafretailinc@gmail.com</a>
-                    </p>
-                    <p>GST/HST Registration No.: <br />727197303RT0001</p>
-					<p>Sales Person: {{$order->sales_manager->name}}</p>
-                </td>
-                <td style="
-              text-align: left;
-              font-size: 24px;
-              font-weight: bold;
-              color: #2a3e10;
-            ">
-                    <p>Order {{$order->id}}</p>
-                </td>
-                <td style="text-align: right; width: 150px">
+                 <td style="text-align: left; width: 150px; padding:40px;">
                     <img src="data:image/jpeg;base64,<?php echo base64_encode(file_get_contents(base_path('public/images/logo.jpg'))); ?>"
                         alt="True Leaf" height="150" width="150" />
+                </td>
+				
+				<td style="text-align: left; width: auto; padding:40px;">
+                    <p class="trueleaf"><strong>True Leaf retail Inc.</strong><br>
+					U - 15, 6720 Davand Drive <br>
+					Mississauga ON L5T 2K7<br>
+                    +1 4377555253<br>
+                     <a href="mailto:trueleafretailinc@gmail.com">trueleafretailinc@gmail.com</a><br>
+                     GST/HST Registration No.:  727197303RT0001<br>
+					 Sales Person: {{$order->sales_manager->name}}</p>
+                </td>
+                
+               
+				
+				<td style=" width: 150px; text-align: right;  font-size:30px;  font-weight: bold;  color: #2A651D;padding:40px;   ">
+                    <p>Order {{$order->id}}</p>
                 </td>
             </tr>
         </table>
@@ -80,45 +88,28 @@
         <table style="width: 100%; " cellpadding="10">
             <tr>
                 <td style="text-align: left;  width:230px;">
-                    <p style="font-size: 14px"><strong>BILL TO</strong></p>
+                    <p style="font-size:16px;"  ><strong>BILL TO</strong></p>
                     <p>{{$order->customer->name}}</p>
                     <p>{{$order->customer->address}}</p>
                 </td>
                 <td style="text-align: left;  width:230px;">
-                    <p style="font-size: 14px"><strong>SHIP TO</strong></p>
+                    <p style="font-size:16px;" ><strong>SHIP TO</strong></p>
                     <p>{{$order->customer->name}}</p>
                     <p>{{$order->customer->address}}</p>
                 </td>
                 <td style="text-align: center">
                     <table style="width: 100%;">
                         <tr>
-                            <td style="
-                    padding: 20px 10px;
-                    vertical-align: middle;
-                    text-align: center;
-                    border-bottom: 1px solid #000000;
-                    background-color: #d4d9cf;
-                    color: #2a3e10;
+                            <td style="  padding: 20px 10px;   vertical-align: middle;  text-align: center;   border-bottom: 1px solid #000000;
+                    background-color: #d4d9cf;  color: #2A651D;
                   ">
                                 <p>DATE<br />{{$order->order_date->format('j F, Y')}}</p>
                             </td>
-                            <td style="
-                    padding: 20px 10px;
-                    vertical-align: middle;
-                    text-align: center;
-                    border-bottom: 1px solid #000000;
-                    background-color: #2a3e10;
-                    color: #ffffff;
+                            <td style="  padding: 20px 10px;   vertical-align: middle;  text-align: center;  border-bottom: 1px solid #000000;   background-color: #2A651D;   color: #ffffff;
                   ">
                                 <p>PLEASE PAY<br /><strong>${{number_format($order->order_total, 2)}}</strong></p>
                             </td>
-                            <td style="
-                    padding: 20px 10px;
-                    vertical-align: middle;
-                    text-align: center;
-                    border-bottom: 1px solid #000000;
-                    background-color: #d4d9cf;
-                    color: #2a3e10;
+                            <td style="  padding: 20px 10px;   vertical-align: middle;  text-align: center;   border-bottom: 1px solid #000000;  background-color:  #d4d9cf;   color: #2A651D;
                   ">
                                 <p>DUE DATE<br />{{$order->due_date->format('j F, Y')}}</p>
                             </td>
@@ -127,10 +118,10 @@
                 </td>
             </tr>
         </table>
-        <table style="width: 100%; border-bottom: 1px solid #000000;" cellpadding="10">
+        <table style="width: 100%; border-bottom: 1px solid grey;" cellpadding="10">
             <tbody>
                 <tr
-                    style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; text-transform: uppercase; font-size: 14px;">
+                    style="border-top: 1px solid grey; border-bottom: 1px solid grey; font-size:14px; text-transform: uppercase;  ">
                     <td style="width: 20%;">
                         <p style="margin-bottom: 0;"><strong>PRODUCT</strong></p>
                     </td>
@@ -151,7 +142,7 @@
                     </td>
                 </tr>
 				@foreach($order->order_item as $item)
-					<tr>
+					<tr class="item-row">
 						<td>
 							<p><strong>{{ $item->name }}</strong></p>
 						</td>
@@ -173,11 +164,11 @@
 								if($item->is_box){
 									$qty = $item->quantity * $item->box_size;
 								}
-								$amount = $qty * $item->sale_price;
+								$amount = $item->quantity * $item->sale_price;
 								
 								$total = $amount + (($amount * $item->tax)/100);
 							@endphp
-							<p>{{ $total }}</p>
+							<p>{{ $amount }}</p>
 						</td>
 					</tr>
 				@endforeach
